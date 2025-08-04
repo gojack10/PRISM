d4666987224e3eefdba4b6afe79080d7e31f9183
gojack10 <gojack10@gmail.com>
2025-08-03 23:51:49 -0700

    test commit

--- src/models/testing/callbacks.py
+++ src/models/testing/callbacks.py
@@ -6,13 +6,11 @@ class LossHistoryCallback(pl.Callback):
         self.val_losses = []
 
     def on_train_epoch_end(self, trainer, pl_module):
-        # Retrieve and store the training loss
         train_loss = trainer.callback_metrics.get('train_loss')
         if train_loss is not None:
             self.train_losses.append(train_loss.item())
 
     def on_validation_epoch_end(self, trainer, pl_module):
-        # Retrieve and store the validation loss
         val_loss = trainer.callback_metrics.get('val_loss')
         if val_loss is not None:
             self.val_losses.append(val_loss.item())
